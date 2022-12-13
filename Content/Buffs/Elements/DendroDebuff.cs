0eb1bdb18a4b963f36444451959a6470e5cb40f7
Jessie Zhong <zhongjessie2003@gmail.com>
2022-12-13 03:40:14 -0800

    Added buffs and helper class for reactions
    very basic skeletons!

---
+++ Content/Buffs/Elements/DendroDebuff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terraria;
+using Terraria.Graphics.Renderers;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Celestia.Content.Buffs.Elements
+{
+    // This class serves as an example of a debuff that causes constant loss of life
+    // See ExampleLifeRegenDebuffPlayer.UpdateBadLifeRegen at the end of the file for more information
+    public class DendroDebuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Electro Debuff"); // Buff display name
+            Description.SetDefault("Losing life"); // Buff description
+            Main.debuff[Type] = true;  // Is it a debuff?
+            Main.pvpBuff[Type] = true; // Players can give other players buffs, which are listed as pvpBuff
+            Main.buffNoSave[Type] = true; // Causes this buff not to persist when exiting and rejoining the world
+            BuffID.Sets.LongerExpertDebuff[Type] = true; // If this buff is a debuff, setting this to true will make this buff last twice as long on players in expert mode
+            BuffID.Sets.IsAnNPCWhipDebuff[Type] = true;
+        }
+    }
+}
---
+++ Helper/BuffHelper.cs
@@ -0,0 +1,195 @@
+﻿using Celestia.Content.Buffs.Elements;
+using Terraria;
+
+namespace Celestia.Helper
+{
+    public static class ReactionHelper
+    {
+        // Each respective detector is called based on elemental application (i.e ElementBuff calls elementDetect).
+        // If another element is detected, remove both elements and apply respective reaction to target NPC
+        public static bool electroDetect(NPC target, Player player)
+        {
+            if (target.HasBuff<PyroDebuff>())
+            {
+                // overload
+                return true;
+            } 
+            else if (target.HasBuff<HydroDebuff>())
+            {
+                // electrocharged
+                return true;
+            } 
+            else if (target.HasBuff<CryoDebuff>())
+            {
+                // superconduct
+                return true;
+            } 
+            else if (target.HasBuff<DendroDebuff>())
+            {
+                // quicken
+                return true;
+            }
+            else if (target.HasBuff<AnemoDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            return false;
+        }
+        public static bool hydroDetect(NPC npc, Player player)
+        {
+            if (npc.HasBuff<PyroDebuff>())
+            {
+                // vaporize 2x
+                return true;
+            }
+            else if (npc.HasBuff<ElectroDebuff>())
+            {
+                // electrocharged
+                return true;
+            }
+            else if (npc.HasBuff<CryoDebuff>())
+            {
+                // frozen
+                return true;
+            }
+            else if (npc.HasBuff<DendroDebuff>())
+            {
+                // bloom
+                return true;
+            }
+            else if (npc.HasBuff<AnemoDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            return false;
+        }
+        public static bool pyroDetect(NPC npc, Player player)
+        {
+            if (npc.HasBuff<ElectroDebuff>())
+            {
+                // overload
+                return true;
+            }
+            else if (npc.HasBuff<HydroDebuff>())
+            {
+                // vaporize 1.5x
+                return true;
+            }
+            else if (npc.HasBuff<CryoDebuff>())
+            {
+                // melt 2x
+                return true;
+            }
+            else if (npc.HasBuff<DendroDebuff>())
+            {
+                // burning
+                return true;
+            }
+            else if (npc.HasBuff<AnemoDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            return false;
+        }
+        public static bool cryoDetect(NPC npc, Player player)
+        {
+            if (npc.HasBuff<PyroDebuff>())
+            {
+                // melt 1.5x
+                return true;
+                return false;
+            }
+            else if (npc.HasBuff<HydroDebuff>())
+            {
+                // frozen
+                return true;
+            }
+            else if (npc.HasBuff<ElectroDebuff>())
+            {
+                // superconduct
+                return true;
+            }
+            else if (npc.HasBuff<DendroDebuff>())
+            {
+                // quicken
+                return true;
+            }
+            else if (npc.HasBuff<AnemoDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            return false;
+        }
+        public static bool dendroDetect(NPC npc, Player player)
+        {
+            if (npc.HasBuff<PyroDebuff>())
+            {
+                // burning
+                return true;
+            }
+            else if (npc.HasBuff<HydroDebuff>())
+            {
+                // bloom
+                return true;
+            }
+            else if (npc.HasBuff<ElectroDebuff>())
+            {
+                // quicken
+                return true;
+            }
+            return false;
+        }
+        public static bool anemoDetect(NPC npc, Player player)
+        {
+            if (npc.HasBuff<PyroDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            else if (npc.HasBuff<HydroDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            else if (npc.HasBuff<CryoDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            else if (npc.HasBuff<DendroDebuff>())
+            {
+                // swirl
+                return true;
+            }
+            return false;
+        }
+        public static bool geoDetect(NPC npc, Player player)
+        {
+            if (npc.HasBuff<PyroDebuff>())
+            {
+                // crystalize
+                return true;
+            }
+            else if (npc.HasBuff<HydroDebuff>())
+            {
+                // crystalize
+                return true;
+            }
+            else if (npc.HasBuff<CryoDebuff>())
+            {
+                // crystalize
+                return true;
+            }
+            else if (npc.HasBuff<DendroDebuff>())
+            {
+                // crystalize
+                return true;
+            }
+            return false;
+        }
+    }
+}
