bb8d94c70f25aef01032af2f8ef6278734ea1813
Jessie Zhong <zhongjessie2003@gmail.com>
2022-12-14 01:01:55 -0800

    added globalnpc for drawing effects

---
+++ Common/GlobalNPCDraw.cs
@@ -0,0 +1,22 @@
+﻿using Celestia.Content.Buffs.Elements;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Celestia.Common
+{
+    public class GlobalNPCDraw : GlobalNPC
+    {
+        public override void DrawEffects(NPC npc, ref Color drawColor)
+        {
+            if (npc.HasBuff<Electro>())
+            {
+                drawColor = Color.Purple;
+            } 
+            else if (npc.HasBuff<Pyro>())
+            {
+                drawColor = Color.OrangeRed;
+            }
+        }
+    }
+}
