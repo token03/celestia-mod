4d91fda953e7abc868727dc1b61f958a5699a8a7
Jessie Zhong <zhongjessie2003@gmail.com>
2022-12-14 01:02:19 -0800

    added static math helper class

---
+++ Helper/MathHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celestia.Helper
+{
+    public static class MathHelper
+    {
+        public static double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        public static double GetRandomNumber(Random random, double minimum, double maximum)
+        {
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+    }
+}
