c190e7219c38a3a51bd2c824610fbbb5dc4b1f01
Jessie Zhong <zhongjessie2003@gmail.com>
2023-01-11 19:19:09 -0800

    premuch finished crystalize.

--- Content/Buffs/Reactions/Crystalize.cs
+++ Content/Buffs/Reactions/Crystalize.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Terraria;
+using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -41,14 +42,24 @@ namespace Celestia.Content.Buffs.Reactions
 
 		public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource, ref int cooldownCounter)
 		{
-			int damageToDeal = Math.Max(damage - ShieldHealth, 0); // reduces damage by shield health and down to 0
+			int damageToDeal = Math.Max(damage - ShieldHealth, 0); // reduces damage by shieldhealth and down to 0
+			
+			if(ShieldHealth > 0)
+			{
+				playSound = false; // no hit sound
+				SoundEngine.PlaySound(SoundID.Item50, Player.position); // plays sound effect
+				Main.NewText("Shield:" + ShieldHealth + "Damage:" + damageToDeal);
+				Player.noKnockback = true;
+				// maybe add sound effect if the shield is broken; maybe also change color of combattext from shield.
+			} else
+				Player.noKnockback = false;
+
 			ShieldHealth = Math.Max(ShieldHealth - damage, 0); //  reduces shieldhealth by damage and down to 0
 			damage = damageToDeal;
-			Main.NewText("Shield:" + ShieldHealth + "  Damage:" + damage);
 			return true; 
-			// bugged to SHIT. will still always deal 1 damage even if zero daamge.
-			// also will apply kncokback. someone else fix this.
-			// Fix it IF YOU DARE!
+
+			// demonic bug; will still always deal 1 damage even if zero daamge.
+			// the shieldhealth handling could probably be cleaner but w/e
 		}
 
 		public override void DrawEffects(PlayerDrawSet drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
